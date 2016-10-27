3d2ef03f303f08cd41c0ea32f8207c2be13bcd11
Nazar <n.suhovich@gmail.com>
2016-10-27 18:42:36 +0300

    Add changes, oss and permissions fragments.

---
+++ app/src/main/java/com/elementary/tasks/navigation/settings/other/PermissionsFragment.java
@@ -0,0 +1,48 @@
+package com.elementary.tasks.navigation.settings.other;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.elementary.tasks.R;
+import com.elementary.tasks.databinding.FragmentSettingsWebViewLayoutBinding;
+import com.elementary.tasks.navigation.settings.BaseSettingsFragment;
+
+/**
+ * Copyright 2016 Nazar Suhovich
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class PermissionsFragment extends BaseSettingsFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentSettingsWebViewLayoutBinding binding = FragmentSettingsWebViewLayoutBinding.inflate(inflater, container, false);
+        String url = "file:///android_asset/files/permissions.html";
+        binding.webView.loadUrl(url);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mCallback != null) {
+            mCallback.onTitleChange(getString(R.string.permissions));
+            mCallback.onFragmentSelect(this);
+        }
+    }
+}
