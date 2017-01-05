eaae67406177c5a72e2ef02c58dc37af25d1de4e
Nazar <n.suhovich@gmail.com>
2017-01-05 18:31:26 +0200

    Add birthday management activity.

---
+++ app/src/main/java/com/elementary/tasks/birthdays/AddBirthdayActivity.java
@@ -0,0 +1,219 @@
+package com.elementary.tasks.birthdays;
+
+import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+
+import com.elementary.tasks.R;
+import com.elementary.tasks.core.ThemedActivity;
+import com.elementary.tasks.core.utils.Constants;
+import com.elementary.tasks.core.utils.Contacts;
+import com.elementary.tasks.core.utils.Permissions;
+import com.elementary.tasks.core.utils.RealmDb;
+import com.elementary.tasks.core.utils.SuperUtil;
+import com.elementary.tasks.databinding.ActivityAddBirthdayBinding;
+
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Copyright 2016 Nazar Suhovich
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class AddBirthdayActivity extends ThemedActivity {
+
+    private static final int MENU_ITEM_DELETE = 12;
+
+    private ActivityAddBirthdayBinding binding;
+
+    private int myYear = 0, myMonth = 0, myDay = 0;
+    private String number;
+    private BirthdayItem mItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String id = intent.getStringExtra(Constants.INTENT_ID);
+        long date = intent.getLongExtra(Constants.INTENT_DATE, 0);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add_birthday);
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        binding.toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        if (themeUtil.isDark()){
+            binding.dateIcon.setImageResource(R.drawable.ic_calendar_white);
+            binding.pickContact.setImageResource(R.drawable.ic_perm_identity_white_24dp);
+        } else {
+            binding.dateIcon.setImageResource(R.drawable.ic_calendar);
+            binding.pickContact.setImageResource(R.drawable.ic_perm_identity_black_24dp);
+        }
+        binding.container.setVisibility(View.GONE);
+        binding.contactCheck.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) binding.container.setVisibility(View.VISIBLE);
+            else binding.container.setVisibility(View.GONE);
+        });
+        mItem = RealmDb.getInstance().getBirthday(id);
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+        if (mItem != null) {
+            binding.birthName.setText(mItem.getName());
+            try {
+                Date dt = CheckBirthdaysAsync.dateFormat.parse(mItem.getDate());
+                if (dt != null) calendar.setTime(dt);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            binding.birthName.setText(mItem.getName());
+            if (!TextUtils.isEmpty(mItem.getNumber())) {
+                binding.phone.setText(mItem.getNumber());
+                binding.contactCheck.setChecked(true);
+            }
+            binding.toolbar.setTitle(R.string.edit_birthday);
+            this.number = mItem.getNumber();
+        } else if (date != 0) {
+            calendar.setTimeInMillis(date);
+        }
+        myYear = calendar.get(Calendar.YEAR);
+        myMonth = calendar.get(Calendar.MONTH);
+        myDay = calendar.get(Calendar.DAY_OF_MONTH);
+        binding.birthDate.setText(CheckBirthdaysAsync.dateFormat.format(calendar.getTime()));
+        binding.birthDate.setOnClickListener(view -> dateDialog());
+        binding.pickContact.setOnClickListener(view -> pickContact());
+    }
+
+    private void pickContact() {
+        if (Permissions.checkPermission(AddBirthdayActivity.this, Permissions.READ_CONTACTS, Permissions.READ_CALLS)) {
+            SuperUtil.selectContact(AddBirthdayActivity.this, Constants.REQUEST_CODE_CONTACTS);
+        } else {
+            Permissions.requestPermission(AddBirthdayActivity.this, 101, Permissions.READ_CONTACTS, Permissions.READ_CALLS);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_group_edit, menu);
+        if (mItem != null) {
+            menu.add(Menu.NONE, MENU_ITEM_DELETE, 100, getString(R.string.delete));
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+                saveBirthday();
+                return true;
+            case android.R.id.home:
+                finish();
+                return true;
+            case MENU_ITEM_DELETE:
+                deleteItem();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveBirthday() {
+        String contact = binding.birthName.getText().toString();
+        if (contact.matches("")) {
+            binding.birthName.setError(getString(R.string.must_be_not_empty));
+            return;
+        }
+        int contactId = Contacts.getIdFromNumber(number, this);
+        if (mItem != null) {
+            mItem.setName(contact);
+            mItem.setContactId(contactId);
+            mItem.setDate(binding.birthDate.getText().toString());
+            mItem.setNumber(number);
+            mItem.setDay(myDay);
+            mItem.setMonth(myMonth);
+        } else {
+            mItem = new BirthdayItem(contact, binding.birthDate.getText().toString().trim(), number, 0, contactId, myDay, myMonth);
+        }
+        RealmDb.getInstance().saveObject(mItem);
+        finish();
+    }
+
+    private void deleteItem() {
+        if (mItem != null) {
+            RealmDb.getInstance().deleteBirthday(mItem);
+        }
+        finish();
+    }
+
+    private void dateDialog() {
+        new DatePickerDialog(this, myDateCallBack, myYear, myMonth, myDay).show();
+    }
+
+    DatePickerDialog.OnDateSetListener myDateCallBack = new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            myYear = year;
+            myMonth = monthOfYear;
+            myDay = dayOfMonth;
+            String monthStr;
+            if (myMonth < 9) {
+                monthStr = "0" + (myMonth + 1);
+            } else monthStr = String.valueOf((myMonth + 1));
+            String dayStr;
+            if (myDay < 10) {
+                dayStr = "0" + myDay;
+            } else dayStr = String.valueOf(myDay);
+            binding.birthDate.setText(SuperUtil.appendString(String.valueOf(myYear), "-", monthStr, "-", dayStr));
+        }
+    };
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        if (requestCode == Constants.REQUEST_CODE_CONTACTS) {
+            if (resultCode == RESULT_OK) {
+                String name = data.getStringExtra(Constants.SELECTED_CONTACT_NAME);
+                number = data.getStringExtra(Constants.SELECTED_CONTACT_NUMBER);
+                if (binding.birthName.getText().toString().matches("")) {
+                    binding.birthName.setText(name);
+                }
+                binding.phone.setText(number);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    SuperUtil.selectContact(AddBirthdayActivity.this, Constants.REQUEST_CODE_CONTACTS);
+                }
+                break;
+        }
+    }
+}
--- app/src/main/java/com/elementary/tasks/core/utils/Constants.java
+++ app/src/main/java/com/elementary/tasks/core/utils/Constants.java
@@ -28,6 +28,7 @@ public class Constants {
     public static final String INTENT_POSITION = "item_position";
     public static final String INTENT_DELETE = "item_delete";
     public static final String INTENT_NOTIFICATION = "item_resumed";
+    public static final String INTENT_DATE = "item_date";
     public static final String SELECTED_CONTACT_NUMBER = "contact_number";
     public static final String SELECTED_TIME = "call_time";
     public static final String FILE_PICKED = "selected_file";
