bd847dc6c8e1d81f57137794d9c1885d79defa3f
Nazar <n.suhovich@gmail.com>
2017-02-15 16:44:42 +0200

    Fix image editing screen.

--- app/src/main/java/com/elementary/tasks/notes/editor/ImageEditActivity.java
+++ app/src/main/java/com/elementary/tasks/notes/editor/ImageEditActivity.java
@@ -155,6 +155,11 @@ public class ImageEditActivity extends ThemedActivity {
     }
 
     @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_palce_edit, menu);
