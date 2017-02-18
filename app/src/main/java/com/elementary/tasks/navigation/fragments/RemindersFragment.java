029ede2ecccf32b9c9ada7ed1b992b2d7a97398a
Nazar <n.suhovich@gmail.com>
2017-02-18 20:06:12 +0200

    Add filterable adapter. Refactor reminders adapter.

--- app/src/main/java/com/elementary/tasks/navigation/fragments/RemindersFragment.java
+++ app/src/main/java/com/elementary/tasks/navigation/fragments/RemindersFragment.java
@@ -20,10 +20,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import com.elementary.tasks.R;
+import com.elementary.tasks.core.adapter.FilterableAdapter;
 import com.elementary.tasks.core.async.SyncTask;
 import com.elementary.tasks.core.controller.EventControl;
 import com.elementary.tasks.core.controller.EventControlFactory;
-import com.elementary.tasks.core.file_explorer.FilterCallback;
 import com.elementary.tasks.core.interfaces.RealmCallback;
 import com.elementary.tasks.core.utils.Constants;
 import com.elementary.tasks.core.utils.DataLoader;
@@ -68,7 +68,6 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
 
     private RemindersRecyclerAdapter mAdapter;
 
-    private List<Reminder> mDataList = new ArrayList<>();
     private ArrayList<String> mGroupsIds;
     private String mLastGroupId;
 
@@ -78,7 +77,7 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
     private SearchView.OnQueryTextListener queryTextListener = new SearchView.OnQueryTextListener() {
         @Override
         public boolean onQueryTextSubmit(String query) {
-            if (mAdapter != null) mAdapter.filter(query, mDataList);
+            if (mAdapter != null) mAdapter.filter(query);
             if (mSearchMenu != null) {
                 mSearchMenu.collapseActionView();
             }
@@ -87,15 +86,8 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
 
         @Override
         public boolean onQueryTextChange(String newText) {
-            if (mAdapter != null) mAdapter.filter(newText, mDataList);
+            if (mAdapter != null) mAdapter.filter(newText);
             return false;
-        }
-    };
-    private FilterCallback mFilterCallback = new FilterCallback() {
-        @Override
-        public void filter(int size) {
-            mRecyclerView.scrollToPosition(0);
-            reloadView();
         }
     };
     private SearchView.OnCloseListener mSearchCloseListener = () -> false;
@@ -107,7 +99,7 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
 
         @Override
         public void onItemClicked(int position, View view) {
-            Reminder reminder = mDataList.get(position);
+            Reminder reminder = mAdapter.getItem(position);
             previewReminder(view, reminder.getUuId(), reminder.getType());
         }
 
@@ -117,15 +109,85 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
         }
     };
     private RealmCallback<List<Reminder>> mLoadCallback = this::showData;
+    private FilterableAdapter.Filter<Reminder, String> mFilter = new FilterableAdapter.Filter<Reminder, String>() {
+        @Override
+        public boolean filter(Reminder reminder, String query) {
+            return reminder.getSummary().toLowerCase().contains(query.toLowerCase());
+        }
+
+        @Override
+        public void onFilterEnd(List<Reminder> list, int size, String query) {
+            mRecyclerView.scrollToPosition(0);
+            reloadView();
+        }
+    };
+
+    @Override
+    public void onActivityCreated(final Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_active_menu, menu);
+        mSearchMenu = menu.findItem(R.id.action_search);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        if (mSearchMenu != null) {
+            mSearchView = (SearchView) mSearchMenu.getActionView();
+        }
+        if (mSearchView != null) {
+            mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+            mSearchView.setOnQueryTextListener(queryTextListener);
+            mSearchView.setOnCloseListener(mSearchCloseListener);
+        }
+        if (mAdapter == null || mAdapter.getItemCount() == 0){
+            menu.findItem(R.id.action_filter).setVisible(false);
+            menu.findItem(R.id.action_search).setVisible(false);
+        } else {
+            menu.findItem(R.id.action_filter).setVisible(true);
+            menu.findItem(R.id.action_search).setVisible(true);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                new SyncTask(mContext, this, false).execute();
+                break;
+            case R.id.action_voice:
+                if (mCallback != null) {
+                    mCallback.onVoiceAction();
+                }
+                break;
+            case R.id.action_filter:
+                filterDialog();
+                break;
+            case R.id.action_exit:
+                getActivity().finish();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentRemindersBinding.inflate(inflater, container, false);
+        initList();
+        return binding.getRoot();
+    }
 
     private void showData(List<Reminder> result) {
-        mDataList = result;
-        mAdapter = new RemindersRecyclerAdapter(mContext, mDataList, mFilterCallback);
+        mAdapter = new RemindersRecyclerAdapter(mContext, result, mFilter);
         mAdapter.setEventListener(mEventListener);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
         mRecyclerView.setAdapter(mAdapter);
         reloadView();
-        getActivity().invalidateOptionsMenu();
     }
 
     private void showActionDialog(int position, View view) {
@@ -165,66 +227,6 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
             Toast.makeText(mContext, R.string.reminder_is_outdated, Toast.LENGTH_SHORT).show();
         }
         loadData(mLastGroupId);
-    }
-
-    @Override
-    public void onActivityCreated(final Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        setHasOptionsMenu(true);
-    }
-
-    @Override
-    public void onCreateOptionsMenu(final Menu menu, final MenuInflater inflater) {
-        inflater.inflate(R.menu.fragment_active_menu, menu);
-        mSearchMenu = menu.findItem(R.id.action_search);
-        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
-        if (mSearchMenu != null) {
-            mSearchView = (SearchView) mSearchMenu.getActionView();
-        }
-        if (mSearchView != null) {
-            mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
-            mSearchView.setOnQueryTextListener(queryTextListener);
-            mSearchView.setOnCloseListener(mSearchCloseListener);
-        }
-        if (mDataList.size() == 0){
-            menu.findItem(R.id.action_filter).setVisible(false);
-            menu.findItem(R.id.action_search).setVisible(false);
-        } else {
-            menu.findItem(R.id.action_filter).setVisible(true);
-            menu.findItem(R.id.action_search).setVisible(true);
-        }
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(final MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_refresh:
-                new SyncTask(mContext, this, false).execute();
-                break;
-            case R.id.action_voice:
-                if (mCallback != null) {
-                    mCallback.onVoiceAction();
-                }
-                break;
-            case R.id.action_filter:
-                filterDialog();
-                break;
-            case R.id.action_exit:
-                getActivity().finish();
-                break;
-            default:
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentRemindersBinding.inflate(inflater, container, false);
-        initList();
-        return binding.getRoot();
     }
 
     private void initList() {
@@ -271,13 +273,13 @@ public class RemindersFragment extends BaseNavigationFragment implements SyncTas
     }
 
     private void reloadView() {
-        int size = mAdapter.getItemCount();
-        if (size > 0){
-            mRecyclerView.setVisibility(View.VISIBLE);
+        if (mAdapter.getItemCount() > 0){
+            if (mRecyclerView.getVisibility() == View.GONE) mRecyclerView.setVisibility(View.VISIBLE);
             binding.emptyItem.setVisibility(View.GONE);
         } else {
             mRecyclerView.setVisibility(View.GONE);
             binding.emptyItem.setVisibility(View.VISIBLE);
+            getActivity().invalidateOptionsMenu();
         }
     }
 
