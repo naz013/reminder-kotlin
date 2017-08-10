d5083e2227e64e1ac239634f7261d5b43bf0e04e
Nazar <n.suhovich@gmail.com>
2017-08-10 15:07:40 +0300

    Add UI test.

---
+++ app/src/androidTest/java/com/elementary/tasks/core/InitTest.java
@@ -0,0 +1,361 @@
+package com.elementary.tasks.core;
+
+
+import android.support.test.espresso.DataInteraction;
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.elementary.tasks.R;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class InitTest {
+
+    @Rule
+    public ActivityTestRule<SplashScreen> mActivityTestRule = new ActivityTestRule<>(SplashScreen.class);
+
+    @Test
+    public void initTest() {
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withId(R.id.next_button), withContentDescription("Next"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction viewPager = onView(
+                allOf(withId(R.id.viewPager),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        viewPager.perform(swipeLeft());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatImageButton2 = onView(
+                allOf(withId(R.id.next_button), withContentDescription("Next"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton2.perform(click());
+
+        ViewInteraction viewPager2 = onView(
+                allOf(withId(R.id.viewPager),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        viewPager2.perform(swipeLeft());
+
+        ViewInteraction appCompatImageButton3 = onView(
+                allOf(withId(R.id.next_button), withContentDescription("Next"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton3.perform(click());
+
+        ViewInteraction viewPager3 = onView(
+                allOf(withId(R.id.viewPager),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        viewPager3.perform(swipeLeft());
+
+        ViewInteraction viewPager4 = onView(
+                allOf(withId(R.id.viewPager),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                3),
+                        isDisplayed()));
+        viewPager4.perform(swipeLeft());
+
+        ViewInteraction appCompatImageButton4 = onView(
+                allOf(withId(R.id.next_button), withContentDescription("Next"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3575204);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.skip_button), withText("Skip"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(android.R.id.button2), withText("Open app"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withClassName(is("android.widget.ScrollView")),
+                                        0),
+                                2)));
+        appCompatButton.perform(scrollTo(), click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3584534);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.fab), withContentDescription("Add"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.drawer_layout),
+                                        0),
+                                1),
+                        isDisplayed()));
+        floatingActionButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(3570140);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(R.id.tv_dismiss), withText("GOT IT"),
+                        childAtPosition(
+                                allOf(withId(R.id.content_box),
+                                        childAtPosition(
+                                                withClassName(is("uk.co.deanwild.materialshowcaseview.MaterialShowcaseView")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(350);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView3 = onView(
+                allOf(withId(R.id.tv_dismiss), withText("GOT IT"),
+                        childAtPosition(
+                                allOf(withId(R.id.content_box),
+                                        childAtPosition(
+                                                withClassName(is("uk.co.deanwild.materialshowcaseview.MaterialShowcaseView")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatTextView3.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(350);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView4 = onView(
+                allOf(withId(R.id.tv_dismiss), withText("GOT IT"),
+                        childAtPosition(
+                                allOf(withId(R.id.content_box),
+                                        childAtPosition(
+                                                withClassName(is("uk.co.deanwild.materialshowcaseview.MaterialShowcaseView")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatTextView4.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(350);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction appCompatTextView5 = onView(
+                allOf(withId(R.id.tv_dismiss), withText("GOT IT"),
+                        childAtPosition(
+                                allOf(withId(R.id.content_box),
+                                        childAtPosition(
+                                                withClassName(is("uk.co.deanwild.materialshowcaseview.MaterialShowcaseView")),
+                                                0)),
+                                2),
+                        isDisplayed()));
+        appCompatTextView5.perform(click());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.navSpinner),
+                        childAtPosition(
+                                allOf(withId(R.id.toolbar),
+                                        childAtPosition(
+                                                withId(R.id.appBar),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        DataInteraction relativeLayout = onData(anything())
+                .inAdapterView(childAtPosition(
+                        withClassName(is("android.widget.PopupWindow$PopupBackgroundView")),
+                        0))
+                .atPosition(1);
+        relativeLayout.perform(click());
+
+        ViewInteraction roboButton = onView(
+                allOf(withText("2"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.timerPickerView),
+                                        2),
+                                1)));
+        roboButton.perform(scrollTo(), click());
+
+        ViewInteraction roboButton2 = onView(
+                allOf(withText("0"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.timerPickerView),
+                                        5),
+                                1)));
+        roboButton2.perform(scrollTo(), click());
+
+        ViewInteraction roboEditText = onView(
+                allOf(withId(R.id.taskSummary),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.appBar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        roboEditText.perform(click());
+
+        ViewInteraction roboEditText2 = onView(
+                allOf(withId(R.id.taskSummary),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.appBar),
+                                        1),
+                                0),
+                        isDisplayed()));
+        roboEditText2.perform(replaceText("test"), closeSoftKeyboard());
+
+        pressBack();
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.action_add), withText("Save"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.toolbar),
+                                        2),
+                                0),
+                        isDisplayed()));
+        actionMenuItemView.perform(click());
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
