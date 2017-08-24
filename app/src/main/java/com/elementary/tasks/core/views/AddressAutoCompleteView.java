a556dda0effa2180cc8b70d7c6fddce4fe753e3b
Nazar <n.suhovich@gmail.com>
2017-08-24 19:45:57 +0300

    Improve address search.

--- app/src/main/java/com/elementary/tasks/core/views/AddressAutoCompleteView.java
+++ app/src/main/java/com/elementary/tasks/core/views/AddressAutoCompleteView.java
@@ -3,19 +3,25 @@ package com.elementary.tasks.core.views;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.location.Address;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatAutoCompleteTextView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.elementary.tasks.core.async.GeocoderTask;
 import com.elementary.tasks.core.utils.AssetsUtil;
 import com.elementary.tasks.core.utils.LogUtil;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -43,36 +49,19 @@ public class AddressAutoCompleteView extends AppCompatAutoCompleteTextView {
     private List<Address> foundPlaces;
 
     private GeocoderTask task;
+    private AddressAdapter mAdapter;
+    private boolean isEnabled = true;
 
     private GeocoderTask.GeocoderListener mExecutionCallback = new GeocoderTask.GeocoderListener() {
         @Override
         public void onAddressReceived(List<Address> addresses) {
             LogUtil.d(TAG, "onAddressReceived: " + addresses);
             foundPlaces = addresses;
-            List<String> namesList = new ArrayList<>();
-            for (Address selected : addresses) {
-                namesList.add(formName(selected));
-            }
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(mContext, android.R.layout.simple_spinner_dropdown_item, namesList);
-            setAdapter(adapter);
-            adapter.notifyDataSetChanged();
+            mAdapter = new AddressAdapter(getContext(), android.R.layout.simple_list_item_2, addresses);
+            setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
         }
     };
-
-    private String formName(Address address) {
-        if (address.getAddressLine(0) != null) {
-            return address.getAddressLine(0);
-        }
-        StringBuilder sb = new StringBuilder();
-        sb.append(address.getFeatureName());
-        if (address.getAdminArea() != null) {
-            sb.append(", ").append(address.getAdminArea());
-        }
-        if (address.getCountryName() != null) {
-            sb.append(", ").append(address.getCountryName());
-        }
-        return sb.toString();
-    }
 
     public AddressAutoCompleteView(Context context) {
         super(context);
@@ -100,7 +89,7 @@ public class AddressAutoCompleteView extends AppCompatAutoCompleteTextView {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                performTypeValue(charSequence.toString());
+                if (isEnabled) performTypeValue(charSequence.toString());
             }
 
             @Override
@@ -136,6 +125,62 @@ public class AddressAutoCompleteView extends AppCompatAutoCompleteTextView {
         super.onAttachedToWindow();
         if (mTypeface != null) {
             setTypeface(mTypeface);
+        }
+    }
+
+    @Override
+    public void setOnItemClickListener(AdapterView.OnItemClickListener l) {
+        super.setOnItemClickListener((adapterView, view, i, l1) -> {
+            if (mAdapter != null) {
+                isEnabled = false;
+                setText(mAdapter.getName(i));
+                isEnabled = true;
+            }
+            if (l != null) l.onItemClick(adapterView, view, i, l1);
+        });
+    }
+
+    private class AddressAdapter extends ArrayAdapter<Address> {
+
+        AddressAdapter(@NonNull Context context, int resource, @NonNull List<Address> objects) {
+            super(context, resource, objects);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                v = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_2, null, false);
+            }
+            TextView tv1 = v.findViewById(android.R.id.text1);
+            TextView tv2 = v.findViewById(android.R.id.text2);
+            Address address = getItem(position);
+            if (address == null) return v;
+            if (address.getAddressLine(0) != null) {
+                tv1.setText(address.getAddressLine(0));
+                tv2.setText(formName(address));
+            } else {
+                tv1.setText(formName(address));
+                tv2.setText("");
+            }
+            return v;
+        }
+
+        public String getName(int position) {
+            return formName(getItem(position));
+        }
+
+        private String formName(Address address) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(address.getFeatureName());
+            if (address.getAdminArea() != null) {
+                sb.append(", ").append(address.getAdminArea());
+            }
+            if (address.getCountryName() != null) {
+                sb.append(", ").append(address.getCountryName());
+            }
+            return sb.toString();
         }
     }
 }
