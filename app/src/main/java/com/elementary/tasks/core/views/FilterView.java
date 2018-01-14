339cba438ca455f40c8f5a6170d74c1852fee876
Nazar Suhovich <n.suhovich@gmail.com>
2018-01-14 12:10:13 +0200

    Start work on new filter view.

--- app/src/main/java/com/elementary/tasks/core/views/FilterView.java
+++ app/src/main/java/com/elementary/tasks/core/views/FilterView.java
@@ -1,17 +1,22 @@
 package com.elementary.tasks.core.views;
 
 import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.HorizontalScrollView;
+import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 import com.elementary.tasks.R;
 import com.elementary.tasks.core.utils.MeasureUtils;
 import com.elementary.tasks.databinding.ChipViewBinding;
+import com.elementary.tasks.databinding.ViewRecyclerBinding;
 
 import java.util.AbstractList;
 import java.util.ArrayList;
@@ -33,9 +38,9 @@ import java.util.List;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 public class FilterView extends LinearLayout {
 
+    private static final String TAG = "FilterView";
     private Context mContext;
     private int numOfFilters;
 
@@ -75,6 +80,7 @@ public class FilterView extends LinearLayout {
         }
         this.addView(createFilter(filter), layoutParams);
         this.numOfFilters++;
+        this.requestLayout();
     }
 
     private void addDivider() {
@@ -87,21 +93,13 @@ public class FilterView extends LinearLayout {
     }
 
     private View createFilter(Filter filter) {
-        HorizontalScrollView scrollView = new HorizontalScrollView(mContext);
-        scrollView.setOverScrollMode(View.OVER_SCROLL_NEVER);
-        scrollView.setHorizontalScrollBarEnabled(false);
-        LinearLayout layout = new LinearLayout(mContext);
-        layout.setOrientation(HORIZONTAL);
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        layoutParams.setMargins(MeasureUtils.dp2px(mContext, 8), 0, 0, 0);
-        for (FilterElement element : filter) {
-            View view = createChip(element, filter.getElementClick());
-            layout.addView(view, layoutParams);
-        }
-        layout.setPadding(0, 0, MeasureUtils.dp2px(mContext, 8), 0);
-        scrollView.addView(layout);
-        return scrollView;
+        ViewRecyclerBinding view = ViewRecyclerBinding.inflate(LayoutInflater.from(getContext()));
+        view.recyclerView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        view.recyclerView.setHorizontalScrollBarEnabled(false);
+        view.recyclerView.setPadding(0, 0, MeasureUtils.dp2px(mContext, 8), 0);
+        view.recyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        view.recyclerView.setAdapter(new FilterAdapter(filter));
+        return view.getRoot();
     }
 
     private View createChip(FilterElement element, FilterElementClick click) {
@@ -117,24 +115,108 @@ public class FilterView extends LinearLayout {
         return binding.getRoot();
     }
 
+    private class FilterAdapter extends RecyclerView.Adapter<FilterAdapter.Holder> {
+        private Filter filter;
+
+        FilterAdapter(Filter filter) {
+            this.filter = filter;
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new Holder(ChipViewBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false).getRoot());
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            Log.d(TAG, "onBindViewHolder: ");
+            FilterElement element = filter.get(position);
+            holder.binding.chipTitle.setText(element.getTitle());
+            if (element.getIconId() == 0) {
+                holder.binding.iconView.setVisibility(GONE);
+            } else {
+                holder.binding.iconView.setImageResource(element.getIconId());
+                holder.binding.iconView.setVisibility(VISIBLE);
+            }
+            if (filter.getChoiceMode() == ChoiceMode.SINGLE) {
+                holder.binding.statusView.setVisibility(GONE);
+            } else {
+                if (element.isChecked) holder.binding.statusView.setVisibility(VISIBLE);
+                else holder.binding.statusView.setVisibility(INVISIBLE);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            Log.d(TAG, "getItemCount: " + filter.size());
+            return filter.size();
+        }
+
+        class Holder extends RecyclerView.ViewHolder {
+            ChipViewBinding binding;
+
+            Holder(View itemView) {
+                super(itemView);
+                binding = DataBindingUtil.bind(itemView);
+                binding.getRoot().setOnClickListener(v -> updateFilter(v, getAdapterPosition()));
+            }
+        }
+
+        private void updateFilter(View v, int position) {
+            if (filter.getChoiceMode() == ChoiceMode.SINGLE) {
+                filter.getElementClick().onClick(v, filter.get(position).id);
+            } else {
+                if (filter.get(position).getId() == 0) {
+                    unCheckAll();
+                }
+                filter.get(position).setChecked(!filter.get(position).isChecked());
+                notifyItemChanged(position);
+                filter.getElementClick().onMultipleSelected(v, getSelected());
+            }
+        }
+
+        private void unCheckAll() {
+            for (FilterElement element : filter) element.setChecked(false);
+            notifyDataSetChanged();
+        }
+
+        private List<Integer> getSelected() {
+            List<Integer> list = new ArrayList<>();
+            for (FilterElement element : filter) if (element.isChecked()) list.add(element.getId());
+            return list;
+        }
+    }
+
     public interface FilterElementClick {
         void onClick(View view, int id);
+
+        void onMultipleSelected(View view, List<Integer> ids);
     }
 
     public static class Filter extends AbstractList<FilterElement> {
-
+        @NonNull
         private FilterElementClick elementClick;
         private List<FilterElement> elements = new ArrayList<>();
+        private ChoiceMode choiceMode = ChoiceMode.SINGLE;
 
-        public Filter(FilterElementClick elementClick) {
+        public Filter(@NonNull FilterElementClick elementClick) {
             this.elementClick = elementClick;
         }
 
-        public FilterElementClick getElementClick() {
+        public ChoiceMode getChoiceMode() {
+            return choiceMode;
+        }
+
+        public void setChoiceMode(ChoiceMode choiceMode) {
+            this.choiceMode = choiceMode;
+        }
+
+        @NonNull
+        FilterElementClick getElementClick() {
             return elementClick;
         }
 
-        public void setElementClick(FilterElementClick elementClick) {
+        public void setElementClick(@NonNull FilterElementClick elementClick) {
             this.elementClick = elementClick;
         }
 
@@ -161,16 +243,31 @@ public class FilterView extends LinearLayout {
     }
 
     public static class FilterElement {
-
         @DrawableRes
         private int iconId;
         private String title;
         private int id;
+        private boolean isChecked;
 
         public FilterElement(@DrawableRes int iconId, String title, int id) {
             this.iconId = iconId;
             this.title = title;
             this.id = id;
+        }
+
+        public FilterElement(@DrawableRes int iconId, String title, int id, boolean isChecked) {
+            this.iconId = iconId;
+            this.title = title;
+            this.id = id;
+            this.isChecked = isChecked;
+        }
+
+        public void setChecked(boolean checked) {
+            isChecked = checked;
+        }
+
+        public boolean isChecked() {
+            return isChecked;
         }
 
         @DrawableRes
@@ -197,5 +294,10 @@ public class FilterView extends LinearLayout {
         public void setId(int id) {
             this.id = id;
         }
+    }
+
+    public enum ChoiceMode {
+        SINGLE,
+        MULTI
     }
 }
