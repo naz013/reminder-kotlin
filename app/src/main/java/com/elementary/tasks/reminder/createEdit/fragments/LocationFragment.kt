4badb369742a918064f596c4244ad43483c7d1c1
Nazar Suhovich <n.suhovich@gmail.com>
2018-09-27 20:33:40 +0300

    Update location reminder type screen.

--- app/src/main/java/com/elementary/tasks/reminder/createEdit/fragments/LocationFragment.kt
+++ app/src/main/java/com/elementary/tasks/reminder/createEdit/fragments/LocationFragment.kt
@@ -5,10 +5,13 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.elementary.tasks.R
 import com.elementary.tasks.core.data.models.Place
 import com.elementary.tasks.core.data.models.Reminder
+import com.elementary.tasks.core.data.models.ReminderGroup
 import com.elementary.tasks.core.fragments.AdvancedMapFragment
 import com.elementary.tasks.core.interfaces.MapCallback
 import com.elementary.tasks.core.interfaces.MapListener
@@ -16,6 +19,7 @@ import com.elementary.tasks.core.utils.*
 import com.elementary.tasks.core.views.ActionView
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.android.synthetic.main.fragment_reminder_location.*
+import timber.log.Timber
 import java.util.*
 
 /**
@@ -48,34 +52,35 @@ class LocationFragment : RadiusTypeFragment() {
         }
 
         override fun onZoomClick(isFull: Boolean) {
-            reminderInterface?.setFullScreenMode(isFull)
+            reminderInterface.setFullScreenMode(isFull)
         }
 
         override fun onBackClick() {
             val map = mAdvancedMapFragment ?: return
             if (map.isFullscreen) {
                 map.isFullscreen = false
-                reminderInterface?.setFullScreenMode(false)
+                reminderInterface.setFullScreenMode(false)
             }
             ViewUtils.fadeOutAnimation(mapContainer)
-            ViewUtils.fadeInAnimation(specsContainer)
+            ViewUtils.fadeInAnimation(scrollView)
         }
     }
 
     private fun showPlaceOnMap() {
-        val iFace = reminderInterface ?: return
-        val reminder = iFace.reminder ?: return
+        val reminder = reminderInterface.reminder
         if (!Reminder.isGpsType(reminder.type)) return
         val text = reminder.summary
-        val jPlace = reminder.places[0]
-        val latitude = jPlace.latitude
-        val longitude = jPlace.longitude
-        radius = jPlace.radius
-        if (mAdvancedMapFragment != null) {
-            mAdvancedMapFragment?.setMarkerRadius(radius)
-            lastPos = LatLng(latitude, longitude)
-            mAdvancedMapFragment?.addMarker(lastPos, text, true, true, radius)
-            toggleMap()
+        if (reminder.places.isNotEmpty()) {
+            val jPlace = reminder.places[0]
+            val latitude = jPlace.latitude
+            val longitude = jPlace.longitude
+            radius = jPlace.radius
+            if (mAdvancedMapFragment != null) {
+                mAdvancedMapFragment?.setMarkerRadius(radius)
+                lastPos = LatLng(latitude, longitude)
+                mAdvancedMapFragment?.addMarker(lastPos, text, true, true, radius)
+                toggleMap()
+            }
         }
     }
 
@@ -84,25 +89,20 @@ class LocationFragment : RadiusTypeFragment() {
     }
 
     override fun prepare(): Reminder? {
-        if (super.prepare() == null) return null
-        val iFace = reminderInterface ?: return null
+        val reminder = super.prepare() ?: return null
         val map = mAdvancedMapFragment ?: return null
         var type = Reminder.BY_LOCATION
         val isAction = actionView.hasAction()
-//        if (TextUtils.isEmpty(iFace.summary) && !isAction) {
-//            iFace.showSnackbar(getString(R.string.task_summary_is_empty))
-//            return null
-//        }
-        val pos = lastPos
-        if (pos == null) {
-            iFace.showSnackbar(getString(R.string.you_dont_select_place))
+        if (TextUtils.isEmpty(reminder.summary) && !isAction) {
+            taskLayout.error = getString(R.string.task_summary_is_empty)
+            taskLayout.isErrorEnabled = true
             return null
         }
         var number = ""
         if (isAction) {
             number = actionView.number
             if (TextUtils.isEmpty(number)) {
-                iFace.showSnackbar(getString(R.string.you_dont_insert_number))
+                reminderInterface.showSnackbar(getString(R.string.you_dont_insert_number))
                 return null
             }
             type = if (actionView.type == ActionView.TYPE_CALL) {
@@ -111,44 +111,29 @@ class LocationFragment : RadiusTypeFragment() {
                 Reminder.BY_LOCATION_SMS
             }
         }
-        var reminder = iFace.reminder
-        if (reminder == null) {
-            reminder = Reminder()
+        val pos = lastPos
+        if (pos == null) {
+            reminderInterface.showSnackbar(getString(R.string.you_dont_select_place))
+            return null
         }
         val places = ArrayList<Place>()
-//        places.add(Place(radius, map.markerStyle, pos.latitude, pos.longitude, iFace.summary, number, listOf()))
+        places.add(Place(radius, map.markerStyle, pos.latitude, pos.longitude, reminder.summary, number, listOf()))
         reminder.places = places
         reminder.target = number
         reminder.type = type
         reminder.exportToCalendar = false
         reminder.exportToTasks = false
+        reminder.hasReminder = attackDelay.isChecked
         if (attackDelay.isChecked) {
             val startTime = dateView.dateTime
             reminder.startTime = TimeUtil.getGmtFromDateTime(startTime)
             reminder.eventTime = TimeUtil.getGmtFromDateTime(startTime)
-            LogUtil.d(TAG, "EVENT_TIME " + TimeUtil.getFullDateTime(startTime, true, true))
+            Timber.d("EVENT_TIME %s", TimeUtil.getFullDateTime(startTime, true, true))
         } else {
             reminder.eventTime = ""
             reminder.startTime = ""
         }
         return reminder
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        setHasOptionsMenu(true)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        inflater!!.inflate(R.menu.fragment_location_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item!!.itemId) {
-            R.id.action_custom_radius -> showRadiusPickerDialog()
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -157,6 +142,11 @@ class LocationFragment : RadiusTypeFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        ViewUtils.listenScrollableView(scrollView) {
+            reminderInterface.updateScroll(it)
+        }
+        moreLayout.isNestedScrollingEnabled = false
+
         val advancedMapFragment = AdvancedMapFragment.newInstance(true, true, true, true,
                 prefs.markerStyle, themeUtil.isDark)
         advancedMapFragment.setListener(mListener)
@@ -172,6 +162,15 @@ class LocationFragment : RadiusTypeFragment() {
 
         this.mAdvancedMapFragment = advancedMapFragment
 
+        if (Module.isPro) {
+            ledView.visibility = View.VISIBLE
+        } else {
+            ledView.visibility = View.GONE
+        }
+
+        tuneExtraView.dialogues = dialogues
+        tuneExtraView.hasAutoExtra = false
+
         actionView.setActivity(activity!!)
         actionView.setContactClickListener(View.OnClickListener { selectContact() })
 
@@ -184,26 +183,92 @@ class LocationFragment : RadiusTypeFragment() {
                 delayLayout.visibility = View.GONE
         }
 
-        clearButton.setOnClickListener { searchField.setText("") }
+        melodyView.onFileSelectListener = {
+            reminderInterface.selectMelody()
+        }
+        attachmentView.onFileSelectListener = {
+            reminderInterface.attachFile()
+        }
+        groupView.onGroupSelectListener = {
+            reminderInterface.selectGroup()
+        }
+
+        clearButton.setOnClickListener { addressField.setText("") }
         mapButton.setOnClickListener { toggleMap() }
-        searchField.setOnItemClickListener { _, _, position, _ ->
-            val sel = searchField.getAddress(position)
+        addressField.setOnItemClickListener { _, _, position, _ ->
+            val sel = addressField.getAddress(position)
             val lat = sel.latitude
             val lon = sel.longitude
             val pos = LatLng(lat, lon)
-//            var title: String? = reminderInterface?.summary
-//            if (title != null && title.matches("".toRegex())) title = pos.toString()
-//            mAdvancedMapFragment?.addMarker(pos, title, true, true, radius)
+            var title: String? = taskSummary.text.toString().trim()
+            if (title != null && title.matches("".toRegex())) title = pos.toString()
+            mAdvancedMapFragment?.addMarker(pos, title, true, true, radius)
         }
+
+        initPropertyFields()
         editReminder()
+    }
+
+    private fun initPropertyFields() {
+        taskSummary.bindProperty(reminderInterface.reminder.summary) {
+            reminderInterface.reminder.summary = it.trim()
+        }
+        dateView.bindProperty(reminderInterface.reminder.eventTime) {
+            reminderInterface.reminder.eventTime = it
+        }
+        priorityView.bindProperty(reminderInterface.reminder.priority) {
+            reminderInterface.reminder.priority = it
+            updateHeader()
+        }
+        actionView.bindProperty(reminderInterface.reminder.target) {
+            reminderInterface.reminder.target = it
+            updateActions()
+        }
+        melodyView.bindProperty(reminderInterface.reminder.melodyPath) {
+            reminderInterface.reminder.melodyPath = it
+        }
+        attachmentView.bindProperty(reminderInterface.reminder.attachmentFile) {
+            reminderInterface.reminder.attachmentFile = it
+        }
+        loudnessView.bindProperty(reminderInterface.reminder.volume) {
+            reminderInterface.reminder.volume = it
+        }
+        windowTypeView.bindProperty(reminderInterface.reminder.windowType) {
+            reminderInterface.reminder.windowType = it
+        }
+        tuneExtraView.bindProperty(reminderInterface.reminder) {
+            reminderInterface.reminder.copyExtra(it)
+        }
+        if (Module.isPro) {
+            ledView.bindProperty(reminderInterface.reminder.color) {
+                reminderInterface.reminder.color = it
+            }
+        }
+    }
+
+    private fun updateActions() {
+        if (actionView.hasAction()) {
+            tuneExtraView.hasAutoExtra = true
+            if (actionView.type == ActionView.TYPE_MESSAGE) {
+                tuneExtraView.hint = getString(R.string.enable_sending_sms_automatically)
+            } else {
+                tuneExtraView.hint = getString(R.string.enable_making_phone_calls_automatically)
+            }
+        } else {
+            tuneExtraView.hasAutoExtra = false
+        }
+    }
+
+    private fun updateHeader() {
+        cardSummary.text = getSummary()
     }
 
     private fun toggleMap() {
         if (mapContainer != null && mapContainer.visibility == View.VISIBLE) {
             ViewUtils.fadeOutAnimation(mapContainer)
-            ViewUtils.fadeInAnimation(specsContainer)
+            ViewUtils.fadeInAnimation(scrollView)
         } else {
-            ViewUtils.fadeOutAnimation(specsContainer)
+            ViewUtils.fadeOutAnimation(scrollView)
             ViewUtils.fadeInAnimation(mapContainer)
             mAdvancedMapFragment?.showShowcase()
         }
@@ -214,9 +279,13 @@ class LocationFragment : RadiusTypeFragment() {
     }
 
     private fun editReminder() {
-        val iFace = reminderInterface ?: return
-        val reminder = iFace.reminder ?: return
-        if (reminder.eventTime != "") {
+        val reminder = reminderInterface.reminder
+        groupView.reminderGroup = ReminderGroup().apply {
+            this.groupColor = reminder.groupColor
+            this.groupTitle = reminder.groupTitle
+            this.groupUuId = reminder.groupUuId
+        }
+        if (reminder.eventTime != "" && reminder.hasReminder) {
             dateView.setDateTime(reminder.eventTime)
             attackDelay.isChecked = true
         }
@@ -260,8 +329,23 @@ class LocationFragment : RadiusTypeFragment() {
         }
     }
 
+    override fun onGroupUpdate(reminderGroup: ReminderGroup) {
+        super.onGroupUpdate(reminderGroup)
+        groupView.reminderGroup = reminderGroup
+        updateHeader()
+    }
+
+    override fun onMelodySelect(path: String) {
+        super.onMelodySelect(path)
+        melodyView.file = path
+    }
+
+    override fun onAttachmentSelect(path: String) {
+        super.onAttachmentSelect(path)
+        attachmentView.file = path
+    }
+
     companion object {
-        private const val TAG = "DateFragment"
         private const val CONTACTS = 122
         const val CONTACTS_ACTION = 123
     }
