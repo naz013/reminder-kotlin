c01534b94407d17edc35c7fc266a78f0cceb5f63
Nazar Suhovich <n.suhovich@gmail.com>
2019-01-08 10:14:23 +0200

    New branded menu icon.

--- app/src/main/java/com/elementary/tasks/navigation/MainActivity.kt
+++ app/src/main/java/com/elementary/tasks/navigation/MainActivity.kt
@@ -127,7 +127,7 @@ class MainActivity : ThemedActivity(), NavigationView.OnNavigationItemSelectedLi
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toolbar.navigationIcon = ViewUtils.tintIcon(this, R.drawable.ic_twotone_menu_24px, isDark)
+        toolbar.navigationIcon = ViewUtils.tintIcon(this, R.drawable.ic_new_menu, isDark)
         toolbar.setNavigationOnClickListener { onDrawerClick() }
     }
 
@@ -213,7 +213,7 @@ class MainActivity : ThemedActivity(), NavigationView.OnNavigationItemSelectedLi
         if (this.fragment is BaseSettingsFragment) {
             toolbar.navigationIcon = ViewUtils.backIcon(this, isDark)
         } else {
-            toolbar.navigationIcon = ViewUtils.tintIcon(this, R.drawable.ic_twotone_menu_24px, isDark)
+            toolbar.navigationIcon = ViewUtils.tintIcon(this, R.drawable.ic_new_menu, isDark)
         }
     }
 
