f8e12c16a67d67225c265657cf9e774d9d46f6bd
Nazar Suhovich <n.suhovich@gmail.com>
2019-01-15 08:44:24 +0200

    Add exporting google task from reminder.

--- app/src/main/java/com/elementary/tasks/core/utils/Constants.kt
+++ app/src/main/java/com/elementary/tasks/core/utils/Constants.kt
@@ -27,6 +27,7 @@ object Constants {
     const val NONE = "none"
     const val DEFAULT = "defaut"
     const val INTENT_ID = "item_id"
+    const val INTENT_JSON = "item_json"
     const val INTENT_IDS = "item_ids"
     const val INTENT_POSITION = "item_position"
     const val INTENT_DELETE = "item_delete"
