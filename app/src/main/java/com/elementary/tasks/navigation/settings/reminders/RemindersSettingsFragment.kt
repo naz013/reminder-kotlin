a4ad4ded2df1234a1a9144ef113455713df80003
Nazar Suhovich <n.suhovich@gmail.com>
2019-01-23 18:58:45 +0200

    Add default priority option to settings.

--- app/src/main/java/com/elementary/tasks/navigation/settings/reminders/RemindersSettingsFragment.kt
+++ app/src/main/java/com/elementary/tasks/navigation/settings/reminders/RemindersSettingsFragment.kt
@@ -2,6 +2,7 @@ package com.elementary.tasks.navigation.settings.reminders
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import com.elementary.tasks.R
 import com.elementary.tasks.core.utils.ViewUtils
 import com.elementary.tasks.navigation.settings.BaseSettingsFragment
@@ -27,6 +28,8 @@ import kotlinx.android.synthetic.main.fragment_settings_reminders.*
  */
 class RemindersSettingsFragment : BaseSettingsFragment() {
 
+    private var mItemSelect: Int = 0
+
     override fun layoutRes(): Int = R.layout.fragment_settings_reminders
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -35,7 +38,37 @@ class RemindersSettingsFragment : BaseSettingsFragment() {
             setScroll(it)
         }
 
+        initDefaultPriority()
         initCompletedPrefs()
+    }
+
+    private fun initDefaultPriority() {
+        defaultPriorityPrefs.setOnClickListener { showPriorityDialog() }
+        showDefaultPriority()
+    }
+
+    private fun showPriorityDialog() {
+        val builder = dialogues.getDialog(context!!)
+        builder.setTitle(getString(R.string.default_priority))
+        val adapter = ArrayAdapter(context!!, android.R.layout.simple_list_item_single_choice, priorityList())
+        mItemSelect = prefs.defaultPriority
+        builder.setSingleChoiceItems(adapter, mItemSelect) { _, which ->
+            mItemSelect = which
+        }
+        builder.setPositiveButton(getString(R.string.ok)) { dialog, _ ->
+            prefs.defaultPriority = mItemSelect
+            dialog.dismiss()
+        }
+        builder.setNegativeButton(R.string.cancel) { dialog, _ ->
+            dialog.dismiss()
+        }
+        val dialog = builder.create()
+        dialog.setOnDismissListener { showDefaultPriority() }
+        dialog.show()
+    }
+
+    private fun showDefaultPriority() {
+        defaultPriorityPrefs.setDetailText(priorityList()[prefs.defaultPriority])
     }
 
     private fun initCompletedPrefs() {
@@ -47,6 +80,16 @@ class RemindersSettingsFragment : BaseSettingsFragment() {
         val isChecked = completedPrefs.isChecked
         completedPrefs.isChecked = !isChecked
         prefs.moveCompleted = !isChecked
+    }
+
+    private fun priorityList(): Array<String> {
+        return arrayOf(
+                getString(R.string.priority_lowest),
+                getString(R.string.priority_low),
+                getString(R.string.priority_normal),
+                getString(R.string.priority_high),
+                getString(R.string.priority_highest)
+        )
     }
 
     override fun getTitle(): String = getString(R.string.reminders_)
