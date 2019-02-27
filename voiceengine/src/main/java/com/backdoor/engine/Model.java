e9511f2c11c03827767947559f4007bf2a76ce22
Nazar Suhovich <n.suhovich@gmail.com>
2019-02-27 19:36:02 +0200

    Increase app version.

--- voiceengine/src/main/java/com/backdoor/engine/Model.java
+++ voiceengine/src/main/java/com/backdoor/engine/Model.java
@@ -21,7 +21,6 @@ import java.util.List;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 public class Model {
 
     private String target;
