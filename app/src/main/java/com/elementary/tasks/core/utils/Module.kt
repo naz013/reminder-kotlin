f0691b52911a8495adb0922a8f997d76d5a7c8f2
Nazar Suhovich <n.suhovich@gmail.com>
2019-02-28 18:48:26 +0200

    Update reminder dialog screen.

--- app/src/main/java/com/elementary/tasks/core/utils/Module.kt
+++ app/src/main/java/com/elementary/tasks/core/utils/Module.kt
@@ -1,27 +1,10 @@
 package com.elementary.tasks.core.utils
 
-import android.content.ComponentName
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.annotation.RequiresApi
 import com.elementary.tasks.BuildConfig
-import com.elementary.tasks.core.app_widgets.buttons.CombinedButtonsWidget
-import com.elementary.tasks.core.app_widgets.buttons.CombinedWidgetConfigActivity
-import com.elementary.tasks.core.app_widgets.buttons.VoiceWidgetDialog
-import com.elementary.tasks.core.app_widgets.calendar.*
-import com.elementary.tasks.core.app_widgets.events.EventsService
-import com.elementary.tasks.core.app_widgets.events.EventsWidget
-import com.elementary.tasks.core.app_widgets.events.EventsWidgetConfigActivity
-import com.elementary.tasks.core.app_widgets.google_tasks.TasksService
-import com.elementary.tasks.core.app_widgets.google_tasks.TasksWidget
-import com.elementary.tasks.core.app_widgets.google_tasks.TasksWidgetConfigActivity
-import com.elementary.tasks.core.app_widgets.notes.NotesService
-import com.elementary.tasks.core.app_widgets.notes.NotesWidget
-import com.elementary.tasks.core.app_widgets.notes.NotesWidgetConfigActivity
-import com.elementary.tasks.core.services.CallReceiver
-import com.elementary.tasks.core.services.GeolocationService
-import timber.log.Timber
 
 /**
  * Copyright 2016 Nazar Suhovich
@@ -91,85 +74,5 @@ object Module {
 
     fun hasBluetooth(context: Context): Boolean {
         return context.packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH)
-    }
-
-    private fun supportWidgets(context: Context): Boolean {
-        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_APP_WIDGETS)
-    }
-
-    fun checkComponents(context: Context) {
-        val pm = context.packageManager
-        Timber.d("checkComponents: $pm")
-
-        if (pm == null) return
-
-        if (Module.hasTelephony(context)) {
-            setState(context, pm, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, CallReceiver::class.java)
-        } else {
-            setState(context, pm, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, CallReceiver::class.java)
-        }
-
-        if (Module.hasLocation(context)) {
-            setState(context, pm, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, GeolocationService::class.java)
-        } else {
-            setState(context, pm, PackageManager.COMPONENT_ENABLED_STATE_DISABLED, GeolocationService::class.java)
-        }
-
-        if (Module.supportWidgets(context)) {
-            setState(context, pm, PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-                    CombinedButtonsWidget::class.java,
-                    CombinedWidgetConfigActivity::class.java,
-                    EventsWidgetConfigActivity::class.java,
-                    NotesWidgetConfigActivity::class.java,
-                    TasksWidgetConfigActivity::class.java,
-                    EventsWidget::class.java,
-                    EventsService::class.java,
-                    NotesWidget::class.java,
-                    NotesService::class.java,
-                    TasksWidget::class.java,
-                    TasksService::class.java,
-                    CalendarWidgetConfigActivity::class.java,
-                    CalendarWidget::class.java,
-                    CalendarWeekdayService::class.java,
-                    CalendarMonthService::class.java,
-                    VoiceWidgetDialog::class.java,
-                    CalendarUpdateMinusService::class.java,
-                    CalendarUpdateService::class.java)
-        } else {
-            setState(context, pm, PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-                    CombinedButtonsWidget::class.java,
-                    CombinedWidgetConfigActivity::class.java,
-                    EventsWidgetConfigActivity::class.java,
-                    NotesWidgetConfigActivity::class.java,
-                    TasksWidgetConfigActivity::class.java,
-                    EventsWidget::class.java,
-                    EventsService::class.java,
-                    NotesWidget::class.java,
-                    NotesService::class.java,
-                    TasksWidget::class.java,
-                    TasksService::class.java,
-                    CalendarWidgetConfigActivity::class.java,
-                    CalendarWidget::class.java,
-                    CalendarWeekdayService::class.java,
-                    CalendarMonthService::class.java,
-                    VoiceWidgetDialog::class.java,
-                    CalendarUpdateMinusService::class.java,
-                    CalendarUpdateService::class.java)
-        }
-    }
-
-    private fun setState(context: Context, pm: PackageManager, state: Int, vararg components: Class<*>) {
-        for (clazz in components) {
-            val componentName = ComponentName(context, clazz)
-            if (!isInSameState(pm, componentName, state)) {
-                pm.setComponentEnabledSetting(componentName, state, PackageManager.DONT_KILL_APP)
-            }
-        }
-    }
-
-    private fun isInSameState(pm: PackageManager, componentName: ComponentName, state: Int): Boolean {
-        val componentEnabledSetting = pm.getComponentEnabledSetting(componentName)
-        Timber.d("isInSameState: component -> $componentName, current -> $componentEnabledSetting, need -> $state")
-        return componentEnabledSetting == state
     }
 }
