ae4a5f7b23b07d523c612a43476cc8945ced2254
Nazar Suhovich <n.suhovich@gmail.com>
2019-03-09 13:34:36 +0200

    Update birthday sorting.

--- app/src/main/java/com/elementary/tasks/birthdays/list/filters/SortModifier.kt
+++ app/src/main/java/com/elementary/tasks/birthdays/list/filters/SortModifier.kt
@@ -3,11 +3,9 @@ package com.elementary.tasks.birthdays.list.filters
 import com.elementary.tasks.core.data.models.Birthday
 import com.elementary.tasks.core.filter.Modifier
 import com.elementary.tasks.core.utils.Prefs
-import com.elementary.tasks.core.utils.TimeCount
 import com.elementary.tasks.core.utils.TimeUtil
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
-import timber.log.Timber
 
 class SortModifier(modifier: Modifier<Birthday>? = null,
                    callback: ((List<Birthday>) -> Unit)? = null) : Modifier<Birthday>(modifier, callback), KoinComponent {
@@ -20,34 +18,6 @@ class SortModifier(modifier: Modifier<Birthday>? = null,
 
     private fun sort(data: List<Birthday>): List<Birthday> {
         val birthTime = TimeUtil.getBirthdayTime(prefs.birthdayTime)
-        data.sortedWith(Comparator { o1, o2 ->
-            if (o1 == null || o2 == null) {
-                when {
-                    o2 != null -> return@Comparator 1
-                    o1 != null -> return@Comparator -1
-                    else -> return@Comparator 0
-                }
-            } else {
-                val t1 = TimeUtil.getFutureBirthdayDate(birthTime, o1.date)
-                val t2 = TimeUtil.getFutureBirthdayDate(birthTime, o2.date)
-                if (t1 == null || t2 == null) {
-                    when {
-                        t2 != null -> return@Comparator 1
-                        t1 != null -> return@Comparator -1
-                        else -> return@Comparator 0
-                    }
-                } else {
-                    val diff1 = TimeCount.getDiffDays(t1.millis)
-                    val diff2 = TimeCount.getDiffDays(t2.millis)
-                    Timber.d("sortBirthdays: $diff1, $diff2")
-                    when {
-                        diff1 > diff2 -> return@Comparator 1
-                        diff2 > diff1 -> return@Comparator -1
-                        else -> return@Comparator 0
-                    }
-                }
-            }
-        })
-        return data
+        return data.asSequence().sortedBy { it.getFutureTime(birthTime) }.toList()
     }
 }
