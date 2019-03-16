d80e417d5a1b21b3481372170c1aa90d87357c91
Nazar Suhovich <n.suhovich@gmail.com>
2019-03-16 11:06:30 +0200

    Fix birthdays permanent refresh.

--- app/src/main/java/com/elementary/tasks/core/view_models/birthdays/BaseBirthdaysViewModel.kt
+++ app/src/main/java/com/elementary/tasks/core/view_models/birthdays/BaseBirthdaysViewModel.kt
@@ -3,11 +3,10 @@ package com.elementary.tasks.core.view_models.birthdays
 import com.elementary.tasks.birthdays.work.DeleteBackupWorker
 import com.elementary.tasks.birthdays.work.SingleBackupWorker
 import com.elementary.tasks.core.data.models.Birthday
-import com.elementary.tasks.core.utils.Constants
-import com.elementary.tasks.core.utils.launchDefault
-import com.elementary.tasks.core.utils.withUIContext
+import com.elementary.tasks.core.utils.*
 import com.elementary.tasks.core.view_models.BaseDbViewModel
 import com.elementary.tasks.core.view_models.Commands
+import org.koin.standalone.inject
 
 /**
  * Copyright 2018 Nazar Suhovich
@@ -29,10 +28,13 @@ import com.elementary.tasks.core.view_models.Commands
  */
 abstract class BaseBirthdaysViewModel : BaseDbViewModel() {
 
+    private val notifier: Notifier by inject()
+
     fun deleteBirthday(birthday: Birthday) {
         postInProgress(true)
         launchDefault {
             appDb.birthdaysDao().delete(birthday)
+            updateBirthdayPermanent()
             startWork(DeleteBackupWorker::class.java, Constants.INTENT_ID, birthday.uuId)
             withUIContext {
                 postInProgress(false)
@@ -41,10 +43,17 @@ abstract class BaseBirthdaysViewModel : BaseDbViewModel() {
         }
     }
 
+    protected fun updateBirthdayPermanent() {
+        if (prefs.isBirthdayPermanentEnabled) {
+            notifier.showBirthdayPermanent()
+        }
+    }
+
     fun saveBirthday(birthday: Birthday) {
         postInProgress(true)
         launchDefault {
             appDb.birthdaysDao().insert(birthday)
+            updateBirthdayPermanent()
             startWork(SingleBackupWorker::class.java, Constants.INTENT_ID, birthday.uuId)
             withUIContext {
                 postInProgress(false)
