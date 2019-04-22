c5fe5c5fa2fc92485937a87db5682851dd09039c
Nazar Suhovich <n.suhovich@gmail.com>
2019-04-22 19:28:05 +0300

    Remove headers.

--- app/src/main/java/com/elementary/tasks/places/create/CreatePlaceActivity.kt
+++ app/src/main/java/com/elementary/tasks/places/create/CreatePlaceActivity.kt
@@ -24,24 +24,6 @@ import com.google.android.gms.maps.model.LatLng
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-/**
- * Copyright 2016 Nazar Suhovich
- *
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 class CreatePlaceActivity : ThemedActivity<ActivityCreatePlaceBinding>(), MapListener, MapCallback {
 
     private lateinit var viewModel: PlaceViewModel
--- app/src/main/java/com/elementary/tasks/reminder/create/fragments/PlacesFragment.kt
+++ app/src/main/java/com/elementary/tasks/reminder/create/fragments/PlacesFragment.kt
@@ -17,24 +17,6 @@ import com.elementary.tasks.databinding.FragmentReminderPlaceBinding
 import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
 
-/**
- * Copyright 2016 Nazar Suhovich
- *
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 class PlacesFragment : RadiusTypeFragment<FragmentReminderPlaceBinding>() {
 
     private var mPlacesMap: PlacesMapFragment? = null
