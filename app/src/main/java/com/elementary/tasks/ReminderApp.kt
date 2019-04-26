0eb6d41c37cdfc232226ceca6efc595bba8ca3fe
Nazar Suhovich <n.suhovich@gmail.com>
2019-04-26 11:35:57 +0300

    Disable leak canary.

--- app/src/main/java/com/elementary/tasks/ReminderApp.kt
+++ app/src/main/java/com/elementary/tasks/ReminderApp.kt
@@ -9,8 +9,6 @@ import com.elementary.tasks.core.services.EventJobService
 import com.elementary.tasks.core.utils.components
 import com.evernote.android.job.JobManager
 import io.fabric.sdk.android.Fabric
-import leakcanary.AndroidExcludedRefs
-import leakcanary.LeakCanary
 import org.koin.android.ext.android.startKoin
 import timber.log.Timber
 
@@ -23,17 +21,17 @@ class ReminderApp : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
-        val prefs = AndroidExcludedRefs.createAppDefaults()
-                .instanceField("android.widget.TextView$3", "this$0")
-                .build()
-        val config = LeakCanary.Config(
-                LeakCanary.config.dumpHeap,
-                prefs,
-                LeakCanary.config.reachabilityInspectorClasses,
-                LeakCanary.config.labelers,
-                LeakCanary.config.computeRetainedHeapSize
-        )
-        LeakCanary.config = config
+//        val prefs = AndroidExcludedRefs.createAppDefaults()
+//                .instanceField("android.widget.TextView$3", "this$0")
+//                .build()
+//        val config = LeakCanary.Config(
+//                LeakCanary.config.dumpHeap,
+//                prefs,
+//                LeakCanary.config.reachabilityInspectorClasses,
+//                LeakCanary.config.labelers,
+//                LeakCanary.config.computeRetainedHeapSize
+//        )
+//        LeakCanary.config = config
         Timber.plant(Timber.DebugTree())
         Fabric.with(this, Crashlytics())
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
