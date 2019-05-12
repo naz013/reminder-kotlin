6981130f9966244c1aa35b3871d8815970e109dd
Nazar Suhovich <n.suhovich@gmail.com>
2019-05-12 12:52:17 +0300

    Update home screen design.

--- app/src/main/java/com/elementary/tasks/core/data/dao/BirthdaysDao.kt
+++ app/src/main/java/com/elementary/tasks/core/data/dao/BirthdaysDao.kt
@@ -9,24 +9,6 @@ import androidx.room.Query
 
 import androidx.room.OnConflictStrategy.REPLACE
 
-/**
- * Copyright 2018 Nazar Suhovich
- *
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 @Dao
 interface BirthdaysDao {
 
@@ -44,6 +26,9 @@ interface BirthdaysDao {
 
     @Query("SELECT * FROM Birthday WHERE dayMonth=:dayMonth")
     fun getAll(dayMonth: String): List<Birthday>
+
+    @Query("SELECT * FROM Birthday WHERE dayMonth=:dayMonth")
+    fun loadAll(dayMonth: String): LiveData<List<Birthday>>
 
     @Insert(onConflict = REPLACE)
     fun insert(birthday: Birthday)
--- app/src/main/java/com/elementary/tasks/experimental/home/HomeViewModel.kt
+++ app/src/main/java/com/elementary/tasks/experimental/home/HomeViewModel.kt
@@ -1,7 +1,13 @@
 package com.elementary.tasks.experimental.home
 
-import androidx.lifecycle.ViewModel
+import com.elementary.tasks.core.utils.TimeUtil
+import com.elementary.tasks.core.view_models.BaseDbViewModel
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel : BaseDbViewModel() {
+
+    val reminders = appDb.reminderDao().loadAllTypesInRange(limit = 3,
+            fromTime = TimeUtil.getDayStart(), toTime = TimeUtil.getDayEnd())
+
+    val birthdays = appDb.birthdaysDao().loadAll(TimeUtil.getBirthdayDayMonth())
 
 }
