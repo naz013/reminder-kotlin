aa759a98fed1dd0bcbedf65f2e504f4ed8ffe668
Nazar Suhovich <n.suhovich@gmail.com>
2019-07-14 11:50:41 +0300

    Update design.

--- app/src/main/java/com/elementary/tasks/core/binding/views/PrefsViewBinding.kt
+++ app/src/main/java/com/elementary/tasks/core/binding/views/PrefsViewBinding.kt
@@ -5,9 +5,11 @@ import android.widget.CheckBox
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatImageView
-import androidx.appcompat.widget.SwitchCompat
 import com.elementary.tasks.R
 import com.elementary.tasks.core.binding.Binding
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textview.MaterialTextView
 
 class PrefsViewBinding(view: View) : Binding(view) {
     val dividerTop: View by bindView(R.id.dividerTop)
@@ -15,11 +17,11 @@ class PrefsViewBinding(view: View) : Binding(view) {
     val iconView: AppCompatImageView by bindView(R.id.iconView)
     val itemsContainer: View by bindView(R.id.itemsContainer)
     val viewContainer: View by bindView(R.id.viewContainer)
-    val prefsPrimaryText: TextView by bindView(R.id.prefsPrimaryText)
-    val prefsSecondaryText: TextView by bindView(R.id.prefsSecondaryText)
-    val prefsCheck: CheckBox by bindView(R.id.prefsCheck)
-    val prefsSwitch: SwitchCompat by bindView(R.id.prefsSwitch)
-    val prefsValue: TextView by bindView(R.id.prefsValue)
+    val prefsPrimaryText: MaterialTextView by bindView(R.id.prefsPrimaryText)
+    val prefsSecondaryText: MaterialTextView by bindView(R.id.prefsSecondaryText)
+    val prefsCheck: MaterialCheckBox by bindView(R.id.prefsCheck)
+    val prefsSwitch: SwitchMaterial by bindView(R.id.prefsSwitch)
+    val prefsValue: MaterialTextView by bindView(R.id.prefsValue)
     val prefsView: AppCompatImageView by bindView(R.id.prefsView)
     val progressView: ProgressBar by bindView(R.id.progressViewPrefs)
 }
