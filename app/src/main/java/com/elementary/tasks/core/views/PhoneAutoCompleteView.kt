873f67800ca3c25524aad6c34a6b4f94a2d01961
Nazar Suhovich <n.suhovich@gmail.com>
2019-07-17 19:23:12 +0300

    Fix email autocomplete.

--- app/src/main/java/com/elementary/tasks/core/views/PhoneAutoCompleteView.kt
+++ app/src/main/java/com/elementary/tasks/core/views/PhoneAutoCompleteView.kt
@@ -1,6 +1,7 @@
 package com.elementary.tasks.core.views
 
 import android.content.Context
+import android.os.Build
 import android.provider.ContactsContract
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,9 +9,12 @@ import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
 import android.widget.BaseAdapter
 import android.widget.Filter
 import android.widget.Filterable
+import android.widget.TextView
 import androidx.appcompat.widget.AppCompatAutoCompleteTextView
 import com.elementary.tasks.core.utils.Permissions
 import com.elementary.tasks.core.utils.launchDefault
@@ -63,6 +67,33 @@ class PhoneAutoCompleteView : AppCompatAutoCompleteTextView {
 
     private fun performTypeValue(s: String) {
         adapter?.filter?.filter(s)
+    }
+
+    override fun getHint(): CharSequence? {
+        return if (isMeizu()) getSuperHintHack()
+        else super.getHint()
+    }
+
+    private fun isMeizu(): Boolean {
+        val manufacturer = Build.MANUFACTURER.toLowerCase(Locale.US)
+        if (manufacturer.contains("meizu")) {
+            return true
+        }
+        return false
+    }
+
+    private fun getSuperHintHack(): CharSequence? {
+        val f = TextView::class.java.getDeclaredField("mHint")
+        f.isAccessible = true
+        return f.get(this) as? CharSequence
+    }
+
+    override fun onCreateInputConnection(outAttrs: EditorInfo?): InputConnection? {
+        return if (isMeizu()) {
+            null
+        } else {
+            super.onCreateInputConnection(outAttrs)
+        }
     }
 
     private inner class PhoneAdapter(items: List<PhoneItem>) : BaseAdapter(), Filterable {
