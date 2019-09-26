2b0e90c4df7a73ade45f17853b930d42c7eb02ea
Nazar Suhovich <n.suhovich@gmail.com>
2019-09-26 10:58:15 +0300

    Maybe fix issue with loading all reminders.

--- app/src/main/java/com/elementary/tasks/core/utils/EnableThread.kt
+++ app/src/main/java/com/elementary/tasks/core/utils/EnableThread.kt
@@ -3,12 +3,18 @@ package com.elementary.tasks.core.utils
 import android.content.Context
 import com.elementary.tasks.core.controller.EventControlFactory
 import com.elementary.tasks.core.data.AppDb
+import com.elementary.tasks.core.data.models.Reminder
 
 object EnableThread {
 
     fun run(context: Context) {
         launchDefault {
-            for (item in AppDb.getAppDatabase(context).reminderDao().getAll(active = true, removed = false)) {
+            val items: List<Reminder> = try {
+                AppDb.getAppDatabase(context).reminderDao().getAll(active = true, removed = false)
+            } catch (e: Exception) {
+                listOf()
+            }
+            for (item in items) {
                 EventControlFactory.getController(item).start()
             }
         }
