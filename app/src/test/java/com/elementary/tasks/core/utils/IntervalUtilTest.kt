1510f21d7e5a8ee6ddc4bead8c678223a9ec1500
Nazar Suhovich <n.suhovich@gmail.com>
2019-12-14 19:44:37 +0200

    Add tests for IntervalUtilTest.kt.

---
+++ app/src/test/java/com/elementary/tasks/core/utils/IntervalUtilTest.kt
@@ -0,0 +1,33 @@
+package com.elementary.tasks.core.utils
+
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class IntervalUtilTest {
+
+    private lateinit var data: List<Int>
+
+    @Before
+    fun setup() {
+        data = IntervalUtil.getWeekRepeat(mon = true, tue = false, wed = false, thu = false, fri = false, sat = false, sun = false)
+    }
+
+    @Test
+    fun getWeekRepeat_shouldReturnSevenItems() {
+        assertEquals(7, data.size)
+    }
+
+    @Test
+    fun getWeekRepeat_shouldHasMondayChecked() {
+        assertEquals(1, data[1])
+    }
+
+    @Test
+    fun isWeekday_shouldReturnTrue() {
+        assertEquals(true, IntervalUtil.isWeekday(data))
+    }
+}
