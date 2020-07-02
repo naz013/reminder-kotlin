016840898380e4835d939e0e39cbda2695b427a0
Nazar Sukhovych <n.suhovich@gmail.com>
2020-07-02 18:20:14 +0200

    Remove files

--- lib_domain/src/main/java/com/github/naz013/reminder/domain/StorageDao.kt
+++
@@ -1,9 +0,0 @@
-package com.github.naz013.reminder.domain
-
-interface StorageDao<T, Id> {
-    suspend fun save(t: T)
-
-    suspend fun get(id: Id): T?
-
-    suspend fun delete(id: Id): Boolean
-}
