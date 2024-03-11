4b9c99bdd9b5e3f27e48ec4a9c311a7c287040ff
Nazar Sukhovych <n.suhovich@gmail.com>
2024-03-11 18:50:47 +0100

    REM-823 - Update Google Task item on Home screen

--- app/src/main/java/com/elementary/tasks/home/scheduleview/viewholder/ScheduleGoogleViewHolderCommon.kt
+++ app/src/main/java/com/elementary/tasks/home/scheduleview/viewholder/ScheduleGoogleViewHolderCommon.kt
@@ -1,9 +1,6 @@
 package com.elementary.tasks.home.scheduleview.viewholder
 
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.widget.TextView
-import androidx.appcompat.widget.AppCompatImageView
 import com.elementary.tasks.core.data.ui.google.UiGoogleTaskList
 
 class ScheduleGoogleViewHolderCommon {
@@ -20,13 +17,5 @@ class ScheduleGoogleViewHolderCommon {
     textView: TextView
   ) {
     textView.text = googleTask.notes
-  }
-
-  fun tintIcon(
-    googleTask: UiGoogleTaskList,
-    imageView: AppCompatImageView
-  ) {
-    val color = googleTask.taskListColor ?: Color.BLACK
-    imageView.imageTintList = ColorStateList.valueOf(color)
   }
 }
