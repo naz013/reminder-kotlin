a9f980f3c6075442d48a94621240afc096ef0a0f
Nazar Sukhovych <n.suhovich@gmail.com>
2024-03-16 13:55:43 +0100

    REM-819 - Update and re-arrange settings screens

--- app/src/main/java/com/elementary/tasks/settings/calendar/CalendarSettingsFragment.kt
+++ app/src/main/java/com/elementary/tasks/settings/calendar/CalendarSettingsFragment.kt
@@ -5,13 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.elementary.tasks.R
+import com.elementary.tasks.core.os.Permissions
+import com.elementary.tasks.core.utils.GoogleCalendarUtils
 import com.elementary.tasks.core.utils.ThemeProvider
+import com.elementary.tasks.core.utils.ui.Dialogues
+import com.elementary.tasks.databinding.DialogWithSeekAndTitleBinding
 import com.elementary.tasks.databinding.FragmentSettingsCalendarBinding
 import com.elementary.tasks.navigation.fragments.BaseSettingsFragment
+import org.koin.android.ext.android.inject
+import java.util.Locale
 
 class CalendarSettingsFragment : BaseSettingsFragment<FragmentSettingsCalendarBinding>() {
 
+  private val googleCalendarUtils by inject<GoogleCalendarUtils>()
+
   private var mItemSelect: Int = 0
+  private var mDataList: MutableList<GoogleCalendarUtils.CalendarItem> = mutableListOf()
+  private val currentPosition: Int
+    get() {
+      return findPosition(mDataList)
+    }
 
   override fun inflate(
     inflater: LayoutInflater,
@@ -39,7 +52,7 @@ class CalendarSettingsFragment : BaseSettingsFragment<FragmentSettingsCalendarBi
     }
     initRemindersColorPrefs()
 
-    binding.themeColorPrefs.setOnClickListener {
+    binding.todayColorPrefs.setOnClickListener {
       showColorPopup(prefs.todayColor, getString(R.string.today_color)) { color ->
         prefs.todayColor = color
         initTodayColorPrefs()
@@ -47,13 +60,18 @@ class CalendarSettingsFragment : BaseSettingsFragment<FragmentSettingsCalendarBi
     }
     initTodayColorPrefs()
 
-    binding.selectedColorPrefs.setOnClickListener {
+    binding.birthdayColorPrefs.setOnClickListener {
       showColorPopup(prefs.birthdayColor, getString(R.string.birthdays_color)) { color ->
         prefs.birthdayColor = color
         initBirthdaysColorPrefs()
       }
     }
     initBirthdaysColorPrefs()
+
+    initExportToCalendarPrefs()
+    initEventDurationPrefs()
+    initSelectCalendarPrefs()
+    initExportToStockPrefs()
   }
 
   private fun showColorPopup(current: Int, title: String, onSave: (Int) -> Unit) {
@@ -125,14 +143,155 @@ class CalendarSettingsFragment : BaseSettingsFragment<FragmentSettingsCalendarBi
   override fun getTitle(): String = getString(R.string.calendar)
 
   private fun initBirthdaysColorPrefs() {
-    binding.selectedColorPrefs.setViewColor(
+    binding.birthdayColorPrefs.setViewColor(
       ThemeProvider.colorBirthdayCalendar(requireContext(), prefs)
     )
   }
 
   private fun initTodayColorPrefs() {
-    binding.themeColorPrefs.setViewColor(
+    binding.todayColorPrefs.setViewColor(
       ThemeProvider.colorCurrentCalendar(requireContext(), prefs)
     )
+  }
+
+  private fun initExportToStockPrefs() {
+    binding.exportToStockPrefs.isChecked = prefs.isStockCalendarEnabled
+    binding.exportToStockPrefs.setOnClickListener { changeExportToStockPrefs() }
+  }
+
+  private fun changeExportToStockPrefs() {
+    val isChecked = binding.exportToStockPrefs.isChecked
+    binding.exportToStockPrefs.isChecked = !isChecked
+    prefs.isStockCalendarEnabled = !isChecked
+  }
+
+  private fun initSelectCalendarPrefs() {
+    binding.selectCalendarPrefs.setOnClickListener { tryToShowSelectCalendarDialog() }
+    binding.selectCalendarPrefs.setDependentView(binding.exportToCalendarPrefs)
+    showCurrentCalendar()
+  }
+
+  private fun initEventDurationPrefs() {
+    binding.eventDurationPrefs.setOnClickListener { showEventDurationDialog() }
+    binding.eventDurationPrefs.setDependentView(binding.exportToCalendarPrefs)
+    showEventDuration()
+  }
+
+  private fun showEventDuration() {
+    binding.eventDurationPrefs.setDetailText(
+      String.format(
+        Locale.getDefault(),
+        getString(R.string.x_minutes),
+        prefs.calendarEventDuration.toString()
+      )
+    )
+  }
+
+  private fun showEventDurationDialog() {
+    val builder = dialogues.getMaterialDialog(requireContext())
+    builder.setTitle(R.string.event_duration)
+    val b = DialogWithSeekAndTitleBinding.inflate(layoutInflater)
+
+    b.seekBar.addOnChangeListener { _, value, _ ->
+      b.titleView.text = String.format(
+        Locale.getDefault(),
+        getString(R.string.x_minutes),
+        value.toInt().toString()
+      )
+    }
+    b.seekBar.stepSize = 1f
+    b.seekBar.valueFrom = 0f
+    b.seekBar.valueTo = 120f
+
+    val duration = prefs.calendarEventDuration
+    b.seekBar.value = duration.toFloat()
+
+    b.titleView.text = String.format(
+      Locale.getDefault(),
+      getString(R.string.x_minutes),
+      duration.toString()
+    )
+    builder.setView(b.root)
+    builder.setPositiveButton(R.string.ok) { _, _ ->
+      prefs.calendarEventDuration = b.seekBar.value.toInt()
+      showEventDuration()
+    }
+    builder.setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
+    val dialog = builder.create()
+    dialog.show()
+    Dialogues.setFullWidthDialog(dialog, requireActivity())
+  }
+
+  private fun changeExportToCalendarPrefs() {
+    permissionFlow.askPermissions(listOf(Permissions.READ_CALENDAR, Permissions.WRITE_CALENDAR)) {
+      val isChecked = binding.exportToCalendarPrefs.isChecked
+      binding.exportToCalendarPrefs.isChecked = !isChecked
+      prefs.isCalendarEnabled = !isChecked
+      if (binding.exportToCalendarPrefs.isChecked && !showSelectCalendarDialog()) {
+        prefs.isCalendarEnabled = false
+        binding.exportToCalendarPrefs.isChecked = false
+      }
+    }
+  }
+
+  private fun tryToShowSelectCalendarDialog() {
+    permissionFlow.askPermissions(listOf(Permissions.READ_CALENDAR, Permissions.WRITE_CALENDAR)) {
+      showSelectCalendarDialog()
+    }
+  }
+
+  private fun showSelectCalendarDialog(): Boolean {
+    mDataList.clear()
+    mDataList.addAll(googleCalendarUtils.getCalendarsList())
+    if (mDataList.isEmpty()) {
+      return false
+    }
+    val names = mDataList.map { it.name }.toTypedArray()
+    val builder = dialogues.getMaterialDialog(requireContext())
+    builder.setTitle(R.string.choose_calendar)
+    mItemSelect = currentPosition
+    builder.setSingleChoiceItems(names, mItemSelect) { _, i ->
+      mItemSelect = i
+    }
+    builder.setPositiveButton(R.string.save) { dialog, _ ->
+      if (mItemSelect != -1 && mItemSelect < mDataList.size) {
+        prefs.defaultCalendarId = mDataList[mItemSelect].id
+      }
+      dialog.dismiss()
+      showCurrentCalendar()
+    }
+    builder.setNegativeButton(R.string.cancel) { dialog, _ ->
+      dialog.dismiss()
+    }
+    builder.create().show()
+    return true
+  }
+
+  private fun showCurrentCalendar() {
+    val calendars = googleCalendarUtils.getCalendarsList()
+    val pos = findPosition(calendars)
+    if (calendars.isNotEmpty() && pos != -1 && pos < calendars.size) {
+      val name = calendars[pos].name
+      binding.selectCalendarPrefs.setDetailText(name)
+    } else {
+      binding.selectCalendarPrefs.setDetailText(null)
+    }
+  }
+
+  private fun initExportToCalendarPrefs() {
+    binding.exportToCalendarPrefs.setOnClickListener { changeExportToCalendarPrefs() }
+    binding.exportToCalendarPrefs.isChecked = prefs.isCalendarEnabled
+  }
+
+  private fun findPosition(list: List<GoogleCalendarUtils.CalendarItem>): Int {
+    if (list.isEmpty()) return -1
+    val id = prefs.defaultCalendarId
+    for (i in list.indices) {
+      val item = list[i]
+      if (item.id == id) {
+        return i
+      }
+    }
+    return -1
   }
 }
