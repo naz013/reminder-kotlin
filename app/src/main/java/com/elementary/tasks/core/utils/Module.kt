9c97ad914f8beff4ecb017b57cafcd5843938940
Nazar Sukhovych <n.suhovich@gmail.com>
2024-12-13 19:56:12 +0100

    REM-871 - Fix build

--- app/src/main/java/com/elementary/tasks/core/utils/Module.kt
+++ app/src/main/java/com/elementary/tasks/core/utils/Module.kt
@@ -9,10 +9,13 @@ object Module {
 
   val currentSdk: Int = Build.VERSION.SDK_INT
   const val minSdk: Int = Build.VERSION_CODES.O
-  const val maxSdk: Int = Build.VERSION_CODES.UPSIDE_DOWN_CAKE
+  const val maxSdk: Int = Build.VERSION_CODES.VANILLA_ICE_CREAM
 
   const val isPro: Boolean = BuildConfig.IS_PRO
   val isDebug: Boolean = BuildConfig.DEBUG
+
+  val is15: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM
+  val is14: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE
   val is13: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
   val is12: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
   val is11: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.R
