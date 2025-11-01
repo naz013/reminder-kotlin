f562e173c6e6be000cd3f23a523506f782391641
Nazar Sukhovych <n.suhovich@gmail.com>
2025-11-02 00:04:35 +0100

    REM-989 - Update Google Tasks screen UI

--- ui-common/src/main/kotlin/com/github/naz013/ui/common/context/ContextExtensions.kt
+++ ui-common/src/main/kotlin/com/github/naz013/ui/common/context/ContextExtensions.kt
@@ -3,11 +3,47 @@ package com.github.naz013.ui.common.context
 import android.content.Context
 import android.content.Intent
 import android.util.TypedValue
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.Px
 import androidx.core.content.ContextCompat
 
 fun Context.colorOf(@ColorRes color: Int) = ContextCompat.getColor(this, color)
+
+/**
+ * Retrieves a color attribute value from the current theme.
+ *
+ * @param attrRes The attribute resource ID (e.g., R.attr.colorSurfaceContainer)
+ * @param defaultColor The default color to return if the attribute is not found
+ * @return The resolved color value as an integer
+ */
+@ColorInt
+fun Context.getThemeColor(@AttrRes attrRes: Int, @ColorInt defaultColor: Int = 0): Int {
+  val typedValue = TypedValue()
+  return if (theme.resolveAttribute(attrRes, typedValue, true)) {
+    typedValue.data
+  } else {
+    defaultColor
+  }
+}
+
+/**
+ * Retrieves a dimension attribute value from the current theme.
+ *
+ * @param attrRes The attribute resource ID (e.g., R.attr.cornerRadiusLarge)
+ * @param defaultDp The default dimension in dp to return if the attribute is not found
+ * @return The resolved dimension value in pixels
+ */
+@Px
+fun Context.getThemeDimension(@AttrRes attrRes: Int, defaultDp: Float = 0f): Float {
+  val typedValue = TypedValue()
+  return if (theme.resolveAttribute(attrRes, typedValue, true)) {
+    TypedValue.complexToDimension(typedValue.data, resources.displayMetrics)
+  } else {
+    dp2px(defaultDp.toInt()).toFloat()
+  }
+}
 
 @Px
 fun Context.dp2px(dp: Int): Int {
