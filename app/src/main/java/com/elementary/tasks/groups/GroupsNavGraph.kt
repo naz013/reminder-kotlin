6647e8a4806f710431846cd14630e808192659ef
Nazar Sukhovych <n.suhovich@gmail.com>
2026-07-12 19:29:35 +0200

    Clean up Group graph

--- app/src/main/java/com/elementary/tasks/groups/GroupsNavGraph.kt
+++ app/src/main/java/com/elementary/tasks/groups/GroupsNavGraph.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.elementary.tasks.R
@@ -17,8 +16,7 @@ import com.elementary.tasks.groups.list.GroupsScreenState
 import com.elementary.tasks.groups.list.GroupsViewModel
 import com.elementary.tasks.navigation.nav3.hideKeyboard
 import com.elementary.tasks.notes.ObserveEvent
-import com.github.naz013.ui.common.Dialogues
-import org.koin.compose.koinInject
+import com.github.naz013.ui.common.compose.foundation.dialog.rememberDialogDispatcher
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 
@@ -35,17 +33,20 @@ fun EntryProviderScope<NavKey>.groupsEntries(backStack: MutableList<NavKey>) {
 @Composable
 private fun GroupsListEntry(backStack: MutableList<NavKey>) {
   val viewModel = koinViewModel<GroupsViewModel>()
-  val dialogues = koinInject<Dialogues>()
-  val context = LocalContext.current
-  val deleteTitle = stringResource(R.string.delete)
+
+  val dialogDispatcher = rememberDialogDispatcher()
+
   viewModel.navigationEvent.ObserveEvent { event ->
     when (event) {
       GroupsViewModel.NavigationEvent.AddGroup -> backStack.add(GroupsNavKey.Edit())
       is GroupsViewModel.NavigationEvent.OpenEdit -> backStack.add(GroupsNavKey.Edit(event.id))
       is GroupsViewModel.NavigationEvent.ConfirmDelete -> {
-        dialogues.askConfirmation(context, deleteTitle) { confirmed ->
-          if (confirmed) viewModel.deleteGroup(event.id)
-        }
+        dialogDispatcher.showDialog(
+          titleRes = R.string.delete_group_permanently,
+          positiveButtonRes = R.string.yes,
+          negativeButtonRes = R.string.cancel,
+          onPositive = { viewModel.deleteGroup(event.id) }
+        )
       }
     }
   }
