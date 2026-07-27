2257a81b4d99b22d118d2a8f91dbf868c173a7ad
Nazar Sukhovych <n.suhovich@gmail.com>
2026-07-27 13:18:36 +0200

    Clean up

--- appwidgets/src/main/kotlin/com/github/naz013/appwidgets/events/UiReminderWidgetListAdapter.kt
+++ appwidgets/src/main/kotlin/com/github/naz013/appwidgets/events/UiReminderWidgetListAdapter.kt
@@ -7,7 +7,6 @@ import com.github.naz013.appwidgets.events.data.UiReminderWidgetList
 import com.github.naz013.appwidgets.events.data.UiReminderWidgetShopList
 import com.github.naz013.appwidgets.events.data.UiShopListWidget
 import com.github.naz013.common.datetime.DateTimeManager
-import com.github.naz013.domain.Reminder
 import com.github.naz013.domain.reminder.v2.ReminderAction
 import com.github.naz013.domain.reminder.v2.ReminderV2
 import org.threeten.bp.LocalDateTime
@@ -69,65 +68,6 @@ internal class UiReminderWidgetListAdapter(
   private fun getDueV2(dateTime: LocalDateTime?): UiReminderDueData {
     val due = dateTime?.let { dateTimeManager.getFullDateTime(it) }
     val dueMillis = dateTime?.let { dateTimeManager.toMillis(it) } ?: 0L
-    return UiReminderDueData(
-      dateTime = due,
-      millis = dueMillis
-    )
-  }
-
-  fun create(data: Reminder): DateSorted {
-    return when {
-      data.readType().hasSubTasks() -> {
-        val due = getDue(data)
-        UiReminderWidgetShopList(
-          uuId = data.uuId,
-          text = data.summary,
-          dateTime = due.dateTime,
-          millis = due.millis.takeIf { it != 0L } ?: Long.MAX_VALUE,
-          items = data.shoppings.map {
-            UiShopListWidget(
-              iconRes = if (it.isChecked) {
-                R.drawable.ic_fluent_checkbox_checked
-              } else {
-                R.drawable.ic_fluent_checkbox_unchecked
-              },
-              text = it.summary
-            )
-          }
-        )
-      }
-
-      Reminder.isBase(data.type, Reminder.BY_LOCATION) ||
-        Reminder.isBase(data.type, Reminder.BY_OUT) ||
-        Reminder.isBase(data.type, Reminder.BY_PLACES) -> {
-        val place = data.places.firstOrNull()?.let {
-          String.format(Locale.getDefault(), "%.5f", it.latitude) + " " +
-            String.format(Locale.getDefault(), "%.5f", it.longitude)
-        }
-        UiReminderWidgetList(
-          uuId = data.uuId,
-          text = data.summary,
-          dateTime = place ?: "",
-          millis = Long.MAX_VALUE
-        )
-      }
-
-      else -> {
-        val due = getDue(data)
-        UiReminderWidgetList(
-          uuId = data.uuId,
-          text = data.summary,
-          dateTime = due.dateTime ?: "",
-          millis = due.millis
-        )
-      }
-    }
-  }
-
-  private fun getDue(data: Reminder): UiReminderDueData {
-    val dateTime = dateTimeManager.fromGmtToLocal(data.eventTime)
-    val dueMillis = dateTimeManager.toMillis(data.eventTime)
-    val due = dateTime?.let { dateTimeManager.getFullDateTime(it) }
     return UiReminderDueData(
       dateTime = due,
       millis = dueMillis
